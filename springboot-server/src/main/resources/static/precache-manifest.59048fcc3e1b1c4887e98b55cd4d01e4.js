55332e1a9698d6b999aadf6646741dbb227a70ef
Charlie Zhang <blokaly@users.noreply.github.com>
2020-07-19 18:28:38 +0800

    package frontend build into springboot server

---
+++ springboot-server/src/main/resources/static/precache-manifest.59048fcc3e1b1c4887e98b55cd4d01e4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df7031f1cc1c559dc93619e5d6285294",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7d9d04f5a020e98e1d94",
+    "url": "/static/css/main.6a44a4a6.chunk.css"
+  },
+  {
+    "revision": "dc2c5ea459e911c88ea7",
+    "url": "/static/js/2.865a3347.chunk.js"
+  },
+  {
+    "revision": "ae2b982d22942e682c22d88ba0dda3e0",
+    "url": "/static/js/2.865a3347.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d9d04f5a020e98e1d94",
+    "url": "/static/js/main.b957e29c.chunk.js"
+  },
+  {
+    "revision": "c81fd9546dc8b9e7dca1",
+    "url": "/static/js/runtime-main.aa982217.js"
+  }
+]);
